4fa46a732c4c7d5d8e2e4744d1f73636198424d2
JoySodaPerf <25600217+JoySodaPerf@users.noreply.github.com>
2023-10-17 11:15:07 +0800

    Add files via upload

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  // $schema: "https://json.schemastore.org/prettierrc",
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  endOfLine: "lf",
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+});
