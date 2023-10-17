317204fde2c07c551d6f31a19ec49bb3ad7f9f93
JoySodaPerf <25600217+JoySodaPerf@users.noreply.github.com>
2023-10-17 13:37:07 +0800

    fix eslint-prettier

--- prettier.config.js
+++ prettier.config.js
@@ -2,14 +2,16 @@
 
 /** @type {import("prettier").Config} */
 const config = {
-  // $schema: "https://json.schemastore.org/prettierrc",
-  useTabs: false,
-  semi: true,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  trailingComma: "all",
-  arrowParens: "always",
-  endOfLine: "lf",
+	// $schema: "https://json.schemastore.org/prettierrc",
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	arrowParens: "always",
+	endOfLine: "lf",
+	// tab缩进大小,默认为2
+	useTabs: true,
+	// tabWidth: 4,
 };
 
 export default config;
